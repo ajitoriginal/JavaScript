41d99b622446033b28a540275e812601cf9e0b32
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-07-20 13:31:56 +0530

    final commit

---
+++ Codevolution/JavaScript Crash Course/index.js
@@ -0,0 +1,52 @@
+/*
+JavaScript:
+    a high level programming language
+    one of the core technologies of the world wide web
+    can be used to build server side applications
+    also used in mobile app development
+    also used to create desktop applications
+    most popular language according to the 2021 Stack Overflow developer survey
+
+How to run JavaScript:
+    Browser
+    Node.js runtime
+
+Data Types:
+    Primitive - String, Number, Boolean, Undefined, Null, BigInt, Symbol
+    Non-Primitive - Objects
+
+Operators:
+    a special symbol used to perform operations on values and variables
+    Types are:
+        Assignment : =
+        Arithmetic : +, -, *, /, %, ++, --
+        Comparision : ==, ===, <, >, <=, >=, !=, !==
+        Logical : &&, ||, !
+        String : + (concatination)
+        Others : Ternary Operator etc. (condition ? true statement : false statement)
+    
+Type Conversions:
+    Implicit Conversion: type coercion where JavaScript itself will automatically convert the type
+    Explicit Conversion: where you manually convert the type
+
+Equality:
+    == (Allows coercion)
+    === (Does not allow coercion)
+
+Conditional Statements:
+    If, else, else if, switch
+
+Looping:
+    For loop, While loop, Do..while loop, For..of loop
+
+Functions:
+    a block of code designed to perform a particular task
+    Ex: Add two numbers, multiply two numbers etc.
+    reusable as they can be defined once and can be called with different values resulting in different results
+    help divide a complex problem into smaller chunks and makes your program easy to understand and maintain
+
+Scope:
+    determines the accessibility or visibility of variables
+    Block scope, Function scope, Global scope
+
+*/
---
+++ Codevolution/JavaScript Crash Course/main.js
@@ -0,0 +1,27 @@
+// console.log("Hello from main.js");
+// let age = 25;
+// console.log(age);
+// let a = null;
+// console.log(a+4)
+// console.log(5+'4')
+// console.log('5'+4)
+// console.log(Boolean(' '))
+// for(let i = 1; i <= 5; i++) {
+//     console.log(`Iteration Number ${i}`)
+// }
+// let i = 1
+// for(; i <= 5;) {
+//     console.log(`Iteration Number ${i}`)
+//      i++
+// }
+if(true) {
+    var myName = 'Ajit'
+    console.log(myName)
+}
+// kk()
+function testFn() {
+    // const myName = 'Singh'
+    console.log(myName)
+}
+
+testFn()
